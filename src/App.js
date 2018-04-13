dda9871ee6deae6a12503f69b9b70504a2ed407c
Rodriguez Elorza <elorzafe@amazon.com>
2018-04-12 22:19:10 -0700

    Updating App.js for awsmobile appsync feature

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "semantic-ui-css/semantic.min.css";
 import 'react-datepicker/dist/react-datepicker.css';
 
 import appSyncConfig from "./AppSync";
+import awsmobile from "./aws-exports";
 import { ApolloProvider } from "react-apollo";
 import AWSAppSyncClient from "aws-appsync";
 import { Rehydrated } from "aws-appsync-react";
@@ -19,6 +20,13 @@ const Home = () => (
     <AllEvents />
   </div>
 );
+
+if (awsmobile.aws_appsync_graphqlEndpoint) { // AppSync API configured with awsmobile-cli
+  appSyncConfig.graphqlEndpoint = awsmobile.aws_appsync_graphqlEndpoint;
+  appSyncConfig.region = awsmobile.aws_appsync_region;
+  appSyncConfig.authenticationType = awsmobile.aws_appsync_authenticationType;
+  appSyncConfig.apiKey = awsmobile.aws_appsync_apiKey;
+}
 
 const App = () => (
   <Router>
